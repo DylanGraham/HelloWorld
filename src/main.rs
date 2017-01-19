e6323364767687c232925d112f3a91f27820a50f
Dylan Graham <dylan.graham@gmail.com>
2017-01-19 11:45:12 +1100

    mutable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-    println!("{}", halve(5.0));
+    println!("{} {} {}", halve(5.0), halve(10.0), halve(15.0));
+    let a = halve(16.0);
+    let mut b = halve(12.0);
+    println!("{} {}", a, b);
+    b = 100.0;
+    println!("{} {}", a, b);
 }
 
 fn halve(x: f32) -> f32 {
